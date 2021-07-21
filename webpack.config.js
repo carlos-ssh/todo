5e0eae54e883fea54801fd058aef259f5ac8a3c0
Carlos-SSH <carlos.anezeh@me.com>
2021-07-21 18:36:57 -0500

    Fix issues in webpack console.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -24,3 +25,6 @@ module.exports = {
     ],
   },
 };
+
+
+    
