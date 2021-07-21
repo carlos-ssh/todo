d29862f8580d7265002dd5a371111f31e71c3070
Carlos-SSH <carlos.anezeh@me.com>
2021-07-21 15:22:06 -0500

    Create and array of items that will displayed into the list.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import './style.css';
+
+const erase = document.querySelector('.erase-container');
+
+const todo = [
+  {
+    description: 'Visit Grand Ma.',
+    completed: false,
+    id: 0,
+    index: 0,
+  },
+  {
+    description: 'Morning Meeting with my Team.',
+    completed: true,
+    id: 1,
+    index: 1,
+  },
+  {
+    description: 'Finish my Capstone Project.',
+    completed: true,
+    id: 2,
+    index: 2,
+  },
+];
