0a85987c5113833f0a87df6b614e3ca691dd574b
Carlos-SSH <carlos.anezeh@me.com>
2021-07-21 17:37:58 -0500

    Added style and fix some elements.

--- src/index.js
+++ src/index.js
@@ -36,7 +36,46 @@ class Todo {
     return this.todo;
   }
 
+showall() {
+    for (let i = 0; i < this.todo.length; i += 1) {
+      const activity = this.todo[i];
+      const container = document.createElement('li');
+      container.classList.add('tdl-element');
 
+      const statusC = document.createElement('div');
+      statusC.classList.add('tdle-status-c');
+
+      const status = document.createElement('i');
+      status.classList.add('far');
+      if (activity.completed === true) {
+        status.classList.add('fa-check-square');
+      } else {
+        status.classList.add('fa-square');
+      }
+      statusC.appendChild(status);
+
+      container.appendChild(statusC);
+
+      const text = document.createElement('div');
+      text.classList.add('tdle-text-c');
+      text.textContent = activity.description;
+
+      container.appendChild(text);
+
+      const lastC = document.createElement('div');
+      lastC.classList.add('tdle-last-c');
+
+      const last = document.createElement('i');
+      last.classList.add('fas');
+      last.classList.add('fa-ellipsis-v');
+
+      lastC.appendChild(last);
+
+      container.appendChild(lastC);
+      erase.parentNode.insertBefore(container, erase);
+    }
+  }
+}
 
 const list = new Todo();
 list.setTodo(todo);
